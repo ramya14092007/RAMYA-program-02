192b0c861612b95084cb7f27a7f3fa8f96e29b6a
RAMYA S <ramya.s978742@gmail.com>
2026-09-25 12:42:03 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE student
+(
+studentID INT(5)PRIMARY KEY,
+studentName VARCHAR(20) NOT NULL,
+DOB DATE NOT NULL,
+Gender VARCHAR(10) NOT NULL,
+DepartmentID INT(5),
+CONSTRAINT UQ_studentNme UNIQUE (studentName),
+CONSTRAINT FK_Department
+FOREIGN KEY (DepartmentID)
+  REFERENCES Department(DepartmentID)
+  );
+  desc student;
